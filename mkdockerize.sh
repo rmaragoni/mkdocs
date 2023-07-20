7327add70f01afc8b40a9493e2bbb74b9be5afa8
Raghu Maragoni <58794200+rmaragoni@users.noreply.github.com>
2023-07-20 22:15:55 +0530

    Creating required directories to produce the tar files

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,9 +2,11 @@
 
 echo "Welcome to dockerized solution for building mkdocs"
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root"
-   exit 1
+if [ -d "mkdocs_project" ]; then
+   echo "Required directory exists..."
+else
+   echo "INFO: Creating directory"	 
+   mkdir mkdocs_project	
 fi
 
 if [ -x "$(command -v docker)" ]; then
@@ -20,11 +22,11 @@ sudo docker build -t mkdocs:v1.0.0 .
 echo "Do you want to produce the sample project and generate site folder?"
 read -p "Continue? (y/n): " confirm
 
-sudo docker run -it --rm -v $PWD:/docs -w /docs -u $(id -u ${USER}):$(id -g ${USER}) mkdocs:v1.0.0 produce
+sudo docker run -it --rm -v $PWD:/docs -w /docs/mkdocs_project -u $(id -u ${USER}):$(id -g ${USER}) mkdocs:v1.0.0 produce
 
 echo "INFO: Site produced successfully, generated tar file can also be hosted in any webserver"
 
 echo "Do you want to serve the webiste with mkdocs generated format?"
 read -p "Continue? (y/n): " confirm
 
-sudo docker run -it --rm -v $PWD:/docs -w /docs -p 8000:8000 -u $(id -u ${USER}):$(id -g ${USER}) mkdocs:v1.0.0 serve
+sudo docker run -it --rm -v $PWD:/docs -w /docs/mkdocs_project -p 8000:8000 -u $(id -u ${USER}):$(id -g ${USER}) mkdocs:v1.0.0 serve
